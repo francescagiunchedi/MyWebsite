99104101636f8511d563b6f3dc464d3a2731f84b
francescagiunchedi <39530831+francescagiunchedi@users.noreply.github.com>
2023-02-13 19:07:00 +0100

    1page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,15 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     fontFamily: {
-      body: "Open Sans",
-      titles: "Montserrat",
+      body: ["Open Sans"],
+      titles: ["Montserrat"],
     },
     extend: {
       colors: {
         primary: "#005ded",
-        seconda:"#00c6fb",
+        seconda: "#00c6fb",
         textcolor: "#353546",
-        background: "#d5d9dd"
-        Borange1: "#f6d365",
-        Borange2: "#fda085",
+        background: "#d5d9dd",
       },
     },
   },
