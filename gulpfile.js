95357781a6e711db0b98361157f67064ada68094
francescagiunchedi <39530831+francescagiunchedi@users.noreply.github.com>
2023-03-08 00:29:51 +0100

    fixing sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("src/index.scss").pipe(sass()).pipe(dest("css"));
+  return src("src/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["src/index.scss"], buildStyles);
+  watch(["src/*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
