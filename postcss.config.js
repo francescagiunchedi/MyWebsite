b12025e29d729ade8f8a9b7e306ffd93744a523a
francescagiunchedi <39530831+francescagiunchedi@users.noreply.github.com>
2023-03-07 16:10:19 +0100

    fixed problem with postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,8 @@
-const cssnano = require("cssnano");
-const postcssPresetEnv = require("postcss-preset-env");
-
 module.exports = {
   plugins: [
-    cssnano({
-      preset: "default",
-    }),
-    postcssPresetEnv({ stage: 1 }),
+    require("postcss-import"),
+    // require("stylelint"),
+    require("postcss-preset-env")({ stage: 1 }),
+    require("cssnano"),
   ],
 };
